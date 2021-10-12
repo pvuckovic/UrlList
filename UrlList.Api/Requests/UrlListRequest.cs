b595051c9f45dabaa69eb375c88b3180ef633d01
tepic992 <72026068+tepic992@users.noreply.github.com>
2021-10-12 16:49:39 +0200

    CrudOperations(Get and Post)-ApiControllers

---
+++ UrlList.Api/Requests/UrlListRequest.cs
@@ -0,0 +1,41 @@
+﻿using ServiceStack.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Sdk;
+using RequiredAttribute = ServiceStack.DataAnnotations.RequiredAttribute;
+using StringLengthAttribute = System.ComponentModel.DataAnnotations.StringLengthAttribute;
+
+namespace UrlList.Api.Requests
+{
+    public class UrlListRequest
+    {
+               
+        public string Title { get; set; }
+
+       
+        public string Description { get; set; }
+       
+        public List<UrlItem> UrlItems { get; set; }
+    }
+
+    public class UrlItem
+    {
+        // [Key]
+        //[DataType(DataType.Text)]
+        public string Url { get; set; }
+
+        //[Required]
+        //[DataType(DataType.Text)]
+        public string Description { get; set; }
+
+        //[StringLength(50, MinimumLength = 10,
+        //ErrorMessage = "Description should be minimum 10 characters and a maximum of 50 characters")]
+        //[DataType(DataType.Text)]
+        public string Title { get; set; }
+    }
+}
--- UrlList.Api/Startup.cs
+++ UrlList.Api/Startup.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace UrlList.Api
 {
     public class Startup
@@ -24,8 +25,9 @@ namespace UrlList.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
+            
+            services.AddControllers();            
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
